d0cb564064d6e71754e4fc71d282b7c96f889923
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-08-29 13:31:54 +0700

    Adding google analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Sora } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 const sora = Sora({ subsets: ["latin"] });
 
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
     description: "Pendekkan URL-mu dengan mudah menggunakan iShort URLs",
     url: "https://ishort.my.id",
     type: "website",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +28,16 @@ export default function RootLayout({
       <body className={`${sora.className} bg-gray-100`}>
         <NextUIProvider>{children}</NextUIProvider>
         <script async src="https://kit.fontawesome.com/c7e6574aa8.js" crossOrigin="anonymous"></script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-GHCHMVJH3X" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-GHCHMVJH3X');
+          `}
+        </Script>
       </body>
     </html>
   );
