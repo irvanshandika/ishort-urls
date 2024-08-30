2c9e1123864fb4dfe319fa87dc38e2c5ef936e04
irvanshandika <shandikamuhammadirvan@gmail.com>
2024-08-30 09:16:33 +0700

    Adding microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,18 @@ export default function RootLayout({
             gtag('config', 'G-GHCHMVJH3X');
           `}
         </Script>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "nv5p14hnsi");
+            `,
+          }}
+        />
       </body>
     </html>
   );
